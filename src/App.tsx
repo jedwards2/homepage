ddcf0744d6a3537085db241442c079d1944af4d0
Jack <jack.morgan.edwards@gmail.com>
2023-04-20 20:15:55 -0400

    styling and links

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ function App() {
         <a href="https://github.com/jedwards2"><img alt="github" src={require("./images/github.png")} /></a>
         <a href="https://www.youtube.com/"><img alt="youtube" src={require("./images/youtube.png")} /></a>
         <a href="https://en.wikipedia.org/wiki/Special:Random"><img alt="wikipedia" src={require("./images/wikipedia.png")} /></a>
-        <a href="https://doc.rust-lang.org/book/title-page.html"><img alt="rustbook" src={require("./images/rust.png")} /></a>
+        <a href="https://www.rust-lang.org/learn"><img alt="rustbook" src={require("./images/rust.png")} /></a>
         <a href="https://www.typescriptlang.org/docs/"><img alt="typescript" src={require("./images/typescript.png")} /></a>
         <a href="https://cycling74.com/forums"><img alt="cycling74 forum" src={require("./images/cycling74.png")} /></a>
       </div>
